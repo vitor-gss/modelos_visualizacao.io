671e192e75503720b4495ac66d45eb2c29158d75
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2026-03-16 15:04:16 -0300

    feat: Adiciona o modelo de Estrelas à página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { ChartBarBig } from "lucide-react";
 import "./App.css";
-// import { Botao } from "./components/Botao";
 import { BotaoCheck } from "./components/BotaoCheck";
 import { Header } from "./components/Header";
 import { ListaBotoes } from "./components/ListaBotoes";
@@ -9,25 +8,39 @@ import { useState, type ElementType } from "react";
 import { GraficoBarraLateral } from "./components/Model/GraficoBarraLateral";
 import { GraficoRadar } from "./components/Model/GraficoRadar";
 import { Grafico } from "./components/Model/Grafico";
+import { ModeloEstrelas } from "./components/Model/ModeloEstrelas";
 
 function App() {
   interface modelosProps {
+    /**
+     * Texto do botão.
+     */
     label: string;
+    /**
+     * Situação do botão.
+     */
     ativo: boolean;
+    /**
+     * O componente ao qual o botão está vinculado.
+     */
     componente: ElementType;
   }
-
+  
   const [modelos, setModelos] = useState<modelosProps[]>([
     { label: "barras", ativo: false, componente: GraficoBarraVertical },
     { label: "barras laterais", ativo: false, componente: GraficoBarraLateral },
     { label: "radar", ativo: false, componente: GraficoRadar },
     // { label: "emojis", ativo: false },
     // { label: "velocímetros", ativo: false },
-    // { label: "estrelas", ativo: false },
+    { label: "estrelas", ativo: false, componente: ModeloEstrelas },
     // { label: "tabelas", ativo: false },
     // { label: "bolhas", ativo: false },
   ]);
 
+  /**
+   * Função que define se o botão de checkbox está ativado ou desativado.
+   * @param label - Texto do checkbox que foi selecionado.
+   */
   const lidarComCheckbox = (label: string) => {
     setModelos((preview) =>
       preview.map((item) =>
@@ -36,16 +49,15 @@ function App() {
     );
   };
 
-  //   setModelos((preview) =>
-  //   preview.includes(label)
-  //     ? preview.filter((item) => item != label)
-  //     : [...preview, label],
-  // );
-
+  /**
+   * Ativa ou desativa todos os botões de checkbox.
+   */
   const ativarTodos = () => {
-    const qtdFalse = modelos.filter((i) => i.ativo == false).length;
+    // Calcula quantos botões estão desativados.
+    const qtdFalse = modelos.filter((item) => item.ativo == false).length;
     setModelos((preview) =>
       preview.map((item) =>
+        // Se a quantidade de botões desativados for menor que o total de botões, ativa todos os botões.
         qtdFalse <= modelos.length && qtdFalse != 0
           ? { ...item, ativo: true }
           : { ...item, ativo: false },
@@ -68,19 +80,19 @@ function App() {
       </ListaBotoes>
       <button
         onClick={ativarTodos}
-        className="bg-violet-600 rounded-full p-4 px-8 w-full sm:max-w-fit
-                    flex flex-row justify-center gap-4
+        className="bg-violet-600 rounded-full p-4 px-8 w-full md:max-w-fit
+                    flex flex-row justify-center place-items-center gap-4
                   hover:bg-violet-700 active:bg-violet-800 active:scale-95
                     ease-in-out duration-200"
       >
-        <ChartBarBig /> Marcar todas as opções
+        <ChartBarBig/> Marcar todas as opções
       </button>
-      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full">
-        {modelos.map((c) => {
-          if (!c.ativo) return null;
-          const GraficoAtual = c.componente;
+      <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 w-full">
+        {modelos.map((component) => {
+          if (!component.ativo) return null;
+          const GraficoAtual = component.componente;
           return (
-            <div className="min-w-0" key={c.label}>
+            <div className="min-w-0" key={component.label}>
               <Grafico>
                 <GraficoAtual />
               </Grafico>
