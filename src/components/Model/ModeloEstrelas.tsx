5265e6b55e4e651cb615460b2ae923d31cf55c75
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2026-03-16 15:05:22 -0300

    feat: Criação do modelo de estrelas

---
+++ src/components/Model/ModeloEstrelas.tsx
@@ -0,0 +1,53 @@
+import { Estrela } from "./Estrela";
+import data from "../../mock/data";
+
+export const ModeloEstrelas = () => {
+  const qntdDados = data.labels.length;
+
+  const calcularPontuacao = (pontuacao: number) => {
+    const qntdEstrelas = Math.floor(pontuacao / 20); // * número mágico
+    return qntdEstrelas;
+  };
+
+  const mostrarEstrelas = (pontuacao: number) => {
+    const qntdEstrelas = calcularPontuacao(pontuacao);
+
+    // const estrelas = [
+    //   <Estrela disabled={true} />,
+    //   <Estrela disabled={true} />,
+    //   <Estrela disabled={true} />,
+    //   <Estrela disabled={true} />,
+    //   <Estrela disabled={true} />,
+    // ];
+
+    // for (let i = 0; i < pontuacao; i++) {
+    //   estrelas.unshift(<Estrela/>);
+    //   estrelas.pop();
+    // }
+
+    const estrelas = Array.from({ length: 5 }, (_, i) => (
+      <Estrela key={i} disabled={i >= qntdEstrelas} />
+    ));
+
+    return <div className="flex flex-row">{estrelas}</div>;
+  };
+
+  const pontuacaoTotal =
+    data.datasets[0].data
+      .map((d) => calcularPontuacao(d))
+      .reduce((a, b) => a + b, 0) / qntdDados;
+
+  return (
+    <div className="flex flex-col h-full justify-between">
+      {data.labels.map((label, i) => (
+        <div className="flex flex-row justify-between" key={label}>
+          <span className="text-black text-sm">{label}</span>
+          <span className="">{mostrarEstrelas(data.datasets[0].data[i])}</span>
+        </div>
+      ))}
+      <span className="text-black">
+        {pontuacaoTotal.toFixed(1)} de média baseada em {qntdDados} tópicos
+      </span>
+    </div>
+  );
+};
