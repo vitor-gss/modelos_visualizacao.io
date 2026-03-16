6b2cb523e60dd9d94a8e92bb63cfd4c9601f19d1
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2026-03-16 15:03:21 -0300

    feat: Criação do component Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header>
-      <h1 className="text-xl md:text-3xl xl:text-5xl">
+      <h1 className="text-2xl md:text-4xl xl:text-5xl">
         Modelos de{" "}
         <span className="bg-linear-to-r from-purple-600 to-violet-500 bg-clip-text text-transparent">
           Visualização
