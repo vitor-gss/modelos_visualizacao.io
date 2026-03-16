dfc8746cdf1b3b0805074f21f233203d35fb9005
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2026-03-16 15:05:06 -0300

    feat: Adiciona o modelo de Radar

--- src/components/Model/GraficoRadar.tsx
+++ src/components/Model/GraficoRadar.tsx
@@ -4,30 +4,12 @@ import ChartDataLabels from "chartjs-plugin-datalabels";
 import data from "../../mock/data";
 
 const datas = {
-  labels: data.labels,
-  datasets: [
-    {
-      label: "Aluno",
-      data: [65, 59, 100, 81, 56, 55, 40],
-      fill: true,
-      backgroundColor: "rgba(255, 99, 132, 0.2)",
-      borderColor: "rgb(255, 99, 132)",
-      pointBackgroundColor: "rgb(255, 99, 132)",
-    },
-    {
-      label: "Turma",
-      data: [28, 48, 40, 19, 96, 27, 100],
-      fill: true,
-      backgroundColor: "rgba(54, 162, 235, 0.2)",
-      borderColor: "rgb(54, 162, 235)",
-      pointBackgroundColor: "rgb(54, 162, 235)",
-    },
-  ],
+  ...data,
   options: {
-  layout: {
-    padding: 30,
-  }
-}
+    layout: {
+      padding: 30,
+    },
+  },
 };
 
 export const GraficoRadar = () => {
