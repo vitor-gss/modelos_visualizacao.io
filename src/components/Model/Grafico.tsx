06ded6012c67892fae55c607011e7ce1f8d755f0
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2026-03-16 15:04:56 -0300

    feat: Cria um componente de container para os modelos de visualização

--- src/components/Model/Grafico.tsx
+++ src/components/Model/Grafico.tsx
@@ -1,5 +1,10 @@
 import type { ReactNode } from "react";
 
+/**
+ * Serve de container para os modelos de visualização.
+ * @param children - Modelo de visualização (Gráfico, Estrela, Gauge, ...) .
+ * @returns Componente TSX.
+ */
 export const Grafico = ({ children }: { children: ReactNode }) => {
-  return <div className="chart-container w-full h-75">{children}</div>;
+  return <div className="chart-container w-full h-100">{children}</div>;
 };
