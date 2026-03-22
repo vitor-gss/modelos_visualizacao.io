0cc990f5bfec96b7b43f443ebcae8a11e3061ca6
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2026-03-22 17:59:39 -0300

    fix: Resolve problema de tipagem em Treemap

--- src/components/Model/ModeloTreemap.tsx
+++ src/components/Model/ModeloTreemap.tsx
@@ -1,50 +1,48 @@
-import { Treemap } from 'recharts'
-import data2 from '../../mock/data2'
+import { Treemap, type TreemapNode } from "recharts";
+import data2 from "../../mock/data2";
 
 export const ModeloTreemap = () => {
+  const CustomContent = (props: TreemapNode) => {
+    const { x, y, width, height, name } = props;
 
-  const CustomContent = (props) => {
-  const { x, y, width, height, name } = props;
+    if (width < 40 || height < 20) return <g />;
 
-  if (width < 40 || height < 20) return null;
+    return (
+      <g>
+        <rect
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          style={{
+            fill: "oklch(65% 0.25 292.717)",
+            stroke: "#fff",
+            strokeWidth: 1,
+          }}
+        />
+        <text
+          x={x + width / 2}
+          y={y + height / 2}
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fill="#fff"
+          fontSize={14}
+          // fontWeight="bold"
+        >
+          {name}
+        </text>
+      </g>
+    );
+  };
 
   return (
-    <g>
-      <rect
-        x={x}
-        y={y}
-        width={width}
-        height={height}
-        style={{
-          fill: 'oklch(65% 0.25 292.717)',
-          stroke: '#fff',
-          strokeWidth: 1,
-        }}
-      />
-      <text
-        x={x + width / 2}
-        y={y + height / 2}
-        textAnchor="middle"
-        dominantBaseline="middle"
-        fill="#fff"
-        fontSize={14}
-        // fontWeight="bold"
-      >
-        {name}
-      </text>
-    </g>
-  );
-};
-
-  return (
-        <Treemap
-      style={{ width: '100%',  aspectRatio: 4 / 3 }}
+    <Treemap
+      style={{ width: "100%", aspectRatio: 4 / 3 }}
       data={data2}
       dataKey="size"
       aspectRatio={4 / 3}
       stroke="#fff"
       content={CustomContent}
-    >
-    </Treemap>
-  )
-}
+    ></Treemap>
+  );
+};
